64c86431489ac773d78ee14e8e7f9ee5591929b7
CN-DXTZ <dengbin1213@gmail.com>
2018-07-26 18:10:29 +0800

    20180726

--- Hard/030_findSubstring.cpp
+++ Hard/030_findSubstring.cpp
@@ -1,17 +1,55 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<unordered_map>
 using namespace std;
+
 class Solution
 {
 public:
-	vector<int> findSubstring(string s, vector<string>& words) 
+	vector<int> findSubstring(string s, vector<string>& words)
 	{
+		vector<int> ans;
+		if (s.empty() || words.empty())
+			return ans;
 
+		int len_s = s.size(), num_word = words.size(), len_word = words[0].size();
+		// 存储单词列表
+		unordered_map<string, int> dict;
+		for (string word : words)
+			dict[word]++;
+
+		// 遍历子串起始位置
+		for (int idx = 0; idx <= len_s - num_word * len_word; idx++)
+		{
+
+			unordered_map<string, int> temp_dict;
+			int t_num_word = 0;
+			// 遍历子串所有单词
+			for (; t_num_word < num_word; t_num_word++)
+			{
+				string curr_word = s.substr(idx + t_num_word * len_word, len_word);
+				if (dict.find(curr_word) != dict.end())
+				{
+					temp_dict[curr_word]++;
+					if (temp_dict[curr_word] > dict[curr_word])
+						break;
+				}
+				else break;
+			}
+			if (t_num_word == num_word)
+				ans.push_back(idx);
+		}
+		return ans;
 	}
 } s;
 
 int main()
 {
+	string s1 = "barfoothefoobarman", s2 = "wordgoodstudentgoodword";
+	vector<string> words1 = { "foo","bar" }, words2 = { "word","student" };
+	s.findSubstring(s1, words1);
+	s.findSubstring(s2, words2);
 
 	system("pause");
 	return 0;
--- Medium/031_nextPermutation.cpp
+++ Medium/031_nextPermutation.cpp
@@ -1,13 +1,45 @@
+// https://leetcode.com/problems/next-permutation/discuss/13867/A-simple-algorithm-from-Wikipedia-with-C++-implementation-(can-be-used-in-Permutations-and-Permutations-II)
 #include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
+
 class Solution
 {
 public:
 	void nextPermutation(vector<int>& nums)
 	{
-		next_permutation(nums.begin(), nums.end());
+		int left = -1;
+		// 倒序找到第一个升序对的位置，即交换可增大字典序
+		for (int i = nums.size() - 2; i >= 0; i--)
+		{
+			if (nums[i] < nums[i + 1])
+			{
+				left = i;
+				break;
+			}
+		}
+		// 若字典序已最大
+		if (left == -1)
+		{
+			reverse(nums.begin(), nums.end());
+			return;
+		}
+
+		// 在上述位置的后续子序列中，找出交换可使字典序增大最小的位置
+		// 由于子序列降序排列，即倒序找出第一个字典序大于上述位置的进行交换
+		// 最后将子序列反转即可
+		int right = -1;
+		for (int i = nums.size() - 1; i > left; i--)
+		{
+			if (nums[i] > nums[left])
+			{
+				right = i;
+				break;
+			}
+		}
+		swap(nums[left], nums[right]);
+		reverse(nums.begin() + left + 1, nums.end());
 	}
 } s;
 
